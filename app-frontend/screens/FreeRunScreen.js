1af4b46e338f554448d2c3760dc6f4952d553aef
Jun0715 <62086447+Jun0715@users.noreply.github.com>
2021-04-22 13:09:07 +0800

    Create top tab navigation

---
+++ app-frontend/screens/FreeRunScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+export default class FreeRunScreen extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <Text>FreeRunScreen</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
