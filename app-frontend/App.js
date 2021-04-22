2082f12af663318dfbb2cc7a294cf2f4d8e7ec28
Error404-FailToLoadName <62086468+Error404-FailToLoadName@users.noreply.github.com>
2021-04-22 14:16:44 +0800

    add hard coded couponScreen

--- app-frontend/App.js
+++ app-frontend/App.js
@@ -6,18 +6,20 @@ import LoginScreen from './screens/LoginScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import AppNavigation from './screens/AppNavigation';
 import eventDetails from "./screens/eventDetails"
+import couponScreen from "./screens/couponScreen"
 
 export default class App extends Component {
     render() {
         return (
             <Router>
                 <Scene key='root'>
-                    <Scene key='splash' component={SplashScreen} initial={false} headerShown={false}></Scene>
+                    <Scene key='splash' component={SplashScreen} initial={true} headerShown={false}></Scene>
                     <Scene key="start" component={StartedScreen} headerShown={false}></Scene>
                     <Scene key="login" component={LoginScreen} headerShown={false}></Scene>
                     <Scene key="register" component={SignUpScreen} headerShown={false}></Scene>
-                    <Scene key="app" component={AppNavigation}  initial={true} headerShown={false}></Scene>
-                    <Scene key="eventDetails" component={eventDetails} headerShown={false}></Scene>
+                    <Scene key="app" component={AppNavigation}  initial={false} headerShown={false}></Scene>
+                    <Scene key="eventDetails" component={eventDetails} initial={false} headerShown={false}></Scene>
+                    <Scene key="coupon" component={couponScreen}  initial={false} headerShown={false}></Scene>
                 </Scene>               
             </Router>
         );
---
+++ app-frontend/screens/couponScreen.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { View, Image, Text, StyleSheet, ScrollView} from 'react-native';
+import { Button } from 'native-base'
+import { Actions } from 'react-native-router-flux';
+import Event from '../images/event.png';
+import Event2 from '../images/marathon.png';
+import Run from '../images/running.jpg';
+//import { createAppContainer } from "react-navigation";
+
+export default class couponScreen extends Component {
+       
+    render() {
+        return (
+            <ScrollView style={styles.background}>
+                <View style={styles.container}>
+                    <View style={styles.coupon}>
+                            <View style={styles.couponDate}>
+                                <Text style={styles.eventDate}>7 July - 12 August 2021</Text>
+                            </View>
+                            <View style={styles.couponRow}>
+                                <View style={styles.couponImg}>
+                                    <Image style={styles.image} source={Event} />
+                                </View>
+                                <View style={styles.couponInfo}>
+                                    <Text style={styles.eventTitle}>Virtual Half Marathon</Text>
+                                    <Text style={styles.eventDatail}>Progress: 2.75 /5km</Text>
+                                </View>
+                            </View>
+                        
+                    </View>
+                    <View style={styles.coupon}>
+                        
+                            <View style={styles.couponDate}>
+                                <Text style={styles.eventDate}>19 May - 25 June 2021</Text>
+                            </View>
+                            <View style={styles.couponRow}>
+                                <View style={styles.couponImg}>
+                                    <Image style={styles.image} source={Event2} />
+                                </View>
+                                <View style={styles.couponInfo}>
+                                    <Text style={styles.eventTitle}>Spartan Virtual Marathon</Text>
+                                    <Text style={styles.eventDatail}>Progress: 13.34 /21km</Text>
+                                </View>
+                            </View>
+                        
+                    </View>
+                    
+                </View>
+            </ScrollView>
+        );
+    }
+}
+
+export const styles = StyleSheet.create({
+    
+    background:{
+        backgroundColor: 'white',
+    },
+    container:{
+        marginTop:"10%",
+    },
+    coupon:{
+        marginTop:"5%",
+        marginBottom:"5%",
+        marginLeft:"10%",
+        marginRight:"10%",
+        justifyContent:"center",
+        flex:1,
+    },
+    couponDate:{
+        marginTop:"5%",
+        marginBottom:"5%",
+        
+    },
+    couponRow:{
+        flexDirection:"row",
+        marginBottom:"5%"
+    },
+    couponImg:{
+        flex:2,
+        marginRight:"5%",
+        height:"100%",
+    },
+    couponInfo:{
+        flex:3,
+        textAlignVertical:"center",
+        justifyContent:"center",
+        height:"100%",
+    },
+    eventDetail:{
+        
+    },
+    eventTitle:{
+        fontWeight:"bold",
+        fontSize:17,
+    },
+    eventDate:{
+        color:"#4d535c",
+    },
+    image: {
+        height:"100%",
+        width:"100%",
+        borderRadius:10,
+    },
+});
+
+
