d58eafd8ed4bf05aaab7ef5de5adbb98219b2f46
Error404-FailToLoadName <62086468+Error404-FailToLoadName@users.noreply.github.com>
2021-04-22 14:33:51 +0800

    update eventDetails

--- app-frontend/screens/eventDetails.js
+++ app-frontend/screens/eventDetails.js
@@ -108,7 +108,9 @@ export const styles = StyleSheet.create({
     },
     infoRow:{
         flexDirection:"row",
-        margin:"10%",
+        marginTop:"5%",
+        marginLeft:"10%",
+        marginRight:"10%",
     },
     eventTitle:{
         textAlign:"left",
@@ -133,7 +135,9 @@ export const styles = StyleSheet.create({
     },
     about:{
         flex:1,
-        margin:"5%",
+        marginTop:"5%",
+        marginLeft:"5%",
+        marginRight:"5%",
     },
     aboutHeading:{
         fontWeight:"bold",
