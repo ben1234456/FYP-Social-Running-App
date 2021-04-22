9afe61df3747e64e01bf4d4363f1604c6e02d251
Error404-FailToLoadName <62086468+Error404-FailToLoadName@users.noreply.github.com>
2021-04-22 15:36:21 +0800

    add hard coded couponDetails

--- app-frontend/App.js
+++ app-frontend/App.js
@@ -8,6 +8,7 @@ import AppNavigation from './screens/AppNavigation';
 import eventDetails from "./screens/eventDetails";
 import TopTabNavigator from "./screens/TopTabNavigator";
 import couponScreen from "./screens/couponScreen"
+import couponDetails from "./screens/couponDetails"
 
 export default class App extends Component {
     render() {
@@ -23,6 +24,7 @@ export default class App extends Component {
                     <Scene key="eventDetails" component={eventDetails} headerShown={false}></Scene>
                     <Scene key="coupon" component={couponScreen}  initial={false} headerShown={false}></Scene>
                     <Scene key="top" component={TopTabNavigator} headerShown={false}></Scene>
+                    <Scene key="couponDetails" component={couponDetails} initial={false} headerShown={false}></Scene>
                 </Scene>               
             </Router>
         );
