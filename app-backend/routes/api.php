7d9e370e7501a278a0269cbd4398590ff51558ef
ben1234456 <benjsl1211@gmail.com>
2021-04-22 13:16:08 +0800

    Controller + Route + Linking

--- app-backend/routes/api.php
+++ app-backend/routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::apiResource('users', UserController::class);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
