40c214e54cb5d05b91538af6a72566b565f91f66
Error404-FailToLoadName <62086468+Error404-FailToLoadName@users.noreply.github.com>
2021-09-22 17:59:56 +0800

    add create/edit/delete/view calendar event list/details

--- app-frontend/screens/editEventCalendarScreen.js
+++ app-frontend/screens/editEventCalendarScreen.js
@@ -1,21 +1,33 @@
 import React,{ Component } from "react";
-import { View,Text,StyleSheet,Image,TextInput,Picker,TouchableOpacity,Button } from "react-native";
+import { View,Text,StyleSheet,Image,TextInput,Picker,TouchableOpacity,Button,Alert } from "react-native";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Icon1 from 'react-native-vector-icons/MaterialIcons';
 import PickerModal from 'react-native-picker-modal-view';
+import TimePicker from "react-native-24h-timepicker";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default class editEventCalendarScreen extends Component{
     constructor(props){
         super(props);
         this.state={
-            eventName:"Join for the Spartan Run!",
-            eventTime:"Tuesday, 10:00AM - 10:30AM",
+            eventName:"",
+            //for display use
+            eventTime:"0000",
+            selectedHour:"0",
+            selectedMin:"00",
+            //converted to int
+            convertedTime:"",
             //default value (empty option)
             remindTime:{"Name":"Add new notifications"},
             remindHour:1,
             remindDay:0,
             remindMinute:0,
             allDay:"ON",
+            userID:"",
+            id:props.route.params.id,
+            
+            date:"",
             //remindtime will be data selected
             data:[{
                 "Name": "15 minutes before",
@@ -47,7 +59,57 @@ export default class editEventCalendarScreen extends Component{
             },
             ],
         };
-        
+        const getData = async () => {
+            //using localhost on IOS and using 10.0.2.2 on Android
+            const baseUrl = Platform.OS === 'android' ? 'http://10.0.2.2:8000' : 'http://localhost';
+            try {
+                const userJson = await AsyncStorage.getItem('@userJson')
+                if (userJson !== null) {
+                    const user = JSON.parse(userJson);
+                    this.setState({
+                        userID: user.id,
+                    });
+                }
+
+            } catch (e) {
+                console.log(e);
+            }
+            fetch(baseUrl + '/api/calendar/'+this.state.id, {
+                method:"GET",
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Successfully get user calendar data')
+                console.log(data)
+                changeToTwoTimeDecimal=(time)=>{
+                    if(time.length==1){
+                        return "0"+time;
+                    }
+                    else{
+                        return time;
+                    }
+                };
+                const hourStart=changeToTwoTimeDecimal(parseInt(data.time/60).toString());
+                const minStart=changeToTwoTimeDecimal((data.time%60).toString());
+                this.setState({
+                    loadedData: data,
+                    eventName:data.title,
+                    selectedHour:hourStart,
+                    selectedMin:minStart,
+                    eventTime:hourStart+minStart,
+                    date:data.date,
+                });
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        }
+
+        getData();
     }
     toggle=()=>{
         if(this.state.allDay=="ON"){
@@ -56,6 +118,13 @@ export default class editEventCalendarScreen extends Component{
         if(this.state.allDay=="OFF"){
             this.setState({ allDay: "ON" });
         }
+        const data = {
+            userID: this.state.userID,
+            time: this.state.eventTime,
+            title: this.state.eventName,
+            date: this.state.date,
+        };
+        console.log(data);
     };
     select=(selected)=>{
         this.setState({ remindTime: selected });
@@ -70,10 +139,93 @@ export default class editEventCalendarScreen extends Component{
     testing=()=>{
         console.log(this.state.remindTime);
     };
+    confirmDelete=()=>{
+        Alert.alert(
+            "Confirm delete",
+            "The delete cannot be undone",
+            [
+                { text: "Ok", onPress: () => this.delete() },
+                { text: "Cancel", style: "cancel"},
+            ]
+        );
+    };
+    delete=()=>{
+        const baseUrl = Platform.OS === 'android' ? 'http://10.0.2.2:8000' : 'http://localhost';
+        fetch( baseUrl + '/api/calendar/'+this.state.id, {
+            method: 'DELETE',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                //success
+                console.log(data)
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        this.props.navigation.navigate('calendarScreen');
+    };
+    save=()=>{
+        const baseUrl = Platform.OS === 'android' ? 'http://10.0.2.2:8000' : 'http://localhost';
+        const data = {
+            userID: this.state.userID,
+            time: this.state.convertedTime,
+            title: this.state.eventName,
+            date: this.state.date,
+        };
+        
+        fetch( baseUrl + '/api/calendar/'+this.state.id, {
+            method: 'PUT',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                //success
+                console.log(data)
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        this.props.navigation.navigate('calendarScreen');
+    }
+    onCancel() {
+        this.TimePicker.close();
+    }
+    
+    onConfirm(hour, minute) {
+        changeToTwoTimeDecimal=(time)=>{
+            if(time.length==1){
+                return "0"+time;
+            }
+            else{
+                return time;
+            }
+        };
+        const convertedTime=(parseInt(hour)*60)+parseInt(minute);
+        const convertedHour=changeToTwoTimeDecimal(hour);
+        this.setState({ 
+            eventTime:convertedHour+minute,
+            convertedTime:convertedTime,
+            selectedHour:hour,
+            selectedMin:minute,
+        });
+        this.TimePicker.close();
+    }
+    
     render(){
-        const { remindTime } = this.state;
         return(
+            
             <View style={styles.container}>
+                {/* <View style={styles.dateContainer}>
+                    <Text style={styles.dateText}>{this.state.date}</Text>
+                </View> */}
                 {/* first row */}
                 <View style={styles.row}>
                     <View style={styles.iconContainer}>
@@ -141,37 +293,52 @@ export default class editEventCalendarScreen extends Component{
                         <View style={styles.infoRowContainer}>
                             <View style={styles.infoContainerLeft}>
                                 <Text>
-                                    Thu,Jun 9,2021
+                                    {this.state.date}
                                 </Text>
                             </View>
-                            <View style={styles.infoContainerRight}>
-                                <Text>
-                                    9:00AM
-                                </Text>
-                            </View>
-                        </View>
-                        <View style={styles.infoRowContainer}>
-                            <View style={styles.infoContainerLeft}>
-                                <Text>
-                                    Fri,Jun 10,2021
-                                </Text>
-                            </View>
-                            <View style={styles.infoContainerRight}>
-                                <Text>
-                                    8:30AM
-                                </Text>
+                            <View style={styles.infoContainerRight1}>
+                                {this.state.allDay=="OFF"
+                                ?
+                                <TouchableOpacity onPress={() => this.TimePicker.open()}>
+                                    <Text>
+                                        {this.state.eventTime}
+                                    </Text>
+                                </TouchableOpacity>
+                                :
+                                <View></View>
+                                }
+                                
                             </View>
                         </View>
                     </View>
                     
                 </View> 
                 <View style={styles.botBtnContainer}>
-                    <TouchableOpacity style={styles.botBtn}>
-                        <Text style={styles.btnText}>
-                            Save
-                        </Text>
+                    <TouchableOpacity  onPress={this.save}>
+                        <View style={styles.botBtn}>
+                            <Text style={styles.btnText}>
+                                Save
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity  onPress={this.confirmDelete}>
+                        <View style={styles.botBtn}>
+                            <Text style={styles.btnText}>
+                                Delete
+                            </Text>
+                        </View>
                     </TouchableOpacity>
                 </View>
+                <TimePicker
+                    ref={ref => {
+                        this.TimePicker = ref;
+                    }}
+                    onCancel={() => this.onCancel()}
+                    selectedHour={this.state.selectedHour}
+                    selectedMinute={this.state.selectedMin}
+                    onConfirm={(hour, minute) => this.onConfirm(hour, minute)}
+                    />
             </View>
             
         );
@@ -212,6 +379,11 @@ export const styles=StyleSheet.create({
         justifyContent:"center",
         flex:2,
     },
+    infoContainerRight1:{
+        justifyContent:"center",
+        flex:2,
+        alignItems:"center",
+    },
     infoText:{
         fontSize:15,
         borderBottomWidth:1,
@@ -223,6 +395,8 @@ export const styles=StyleSheet.create({
         alignItems:"center",
         paddingTop:"1%",
         paddingBottom:"1%",
+        borderColor:"#8352F2",
+        borderWidth:1
     },
     toggled:{
         backgroundColor:"white",
@@ -244,16 +418,30 @@ export const styles=StyleSheet.create({
         marginBottom:"5%"
     },
     botBtnContainer:{
-        justifyContent:"flex-end",
         flex:1,
+        justifyContent:"center",
+        alignItems:"flex-end",
+        flexDirection:"row",
     },
     botBtn:{
         alignItems:"center",
+        flex:1,
         backgroundColor:"#8352F2",
         borderRadius:30,
         padding:"5%",
+        margin:"2.5%",
     },
     btnText:{
         color:"white",
+    },
+    dateContainer:{
+        justifyContent:"center",
+        alignItems:"center",
+        
+        paddingBottom:"10%",
+    },
+    dateText:{
+        fontSize:25,
+        fontWeight:"bold",
     },
 });
